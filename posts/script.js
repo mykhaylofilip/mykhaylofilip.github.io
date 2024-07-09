c1802e0732c1147c93f12528044baf12eea26155
Mykhaylo Filip <solid.state.drivers@gmail.com>
2024-07-09 17:27:23 -0400

    Create script.js

---
+++ posts/script.js
@@ -0,0 +1,9 @@
+function navigateTo(section) {
+  if (section === 'home') {
+    window.location.href = 'https://mykhaylofilip.com';
+  } else if (section === 'posts') {
+    window.location.href = 'https://mykhaylofilip.com/posts';
+  } else if (section === 'other') {
+    window.location.href = 'https://mykhaylofilip.com/etc';
+  }
+}
