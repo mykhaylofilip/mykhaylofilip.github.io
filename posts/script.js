947e8b3d376400efff6ce913a5f81b4c610d5f0c
Mykhaylo Filip <solid.state.drivers@gmail.com>
2024-07-09 17:38:09 -0400

    Delete posts/script.js

--- posts/script.js
+++
@@ -1,9 +0,0 @@
-function navigateTo(section) {
-  if (section === 'home') {
-    window.location.href = 'https://mykhaylofilip.com';
-  } else if (section === 'posts') {
-    window.location.href = 'https://mykhaylofilip.com/posts';
-  } else if (section === 'other') {
-    window.location.href = 'https://mykhaylofilip.com/etc';
-  }
-}
